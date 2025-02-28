1624b454fdc5bad01876d9d1e04fdf01fe9a9f5a
hykk-git <gratia1206@naver.com>
2025-02-28 08:00:06 +0000

    예제 연습

--- shoot/shootgame/prac.py
+++ shoot/shootgame/prac.py
@@ -1,8 +1,4 @@
 from django.db import models
 
-class Phone:
-    result = models.IntegerField()
-    
-    calls = models.TextField()
-    for call in calls:
-        results = == 
+class Event:
+    pass
