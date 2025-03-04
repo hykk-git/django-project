12d1cde47fb34c3b458ebe63bce9c9c2232a6a6d
hykk-git <gratia1206@naver.com>
2025-03-04 08:36:01 +0000

    view 추가

--- shoot/shootgame/models.py
+++ shoot/shootgame/models.py
@@ -19,42 +19,45 @@ class NonVisible(GameObject):
     class Meta:
         abstract = True
 
-class Collisible(Visible):
+class Collapsible(Visible):
     __speed = models.IntegerField()
     __angle = models.IntegerField()
 
     def isCollision(self, unit):
+        # 충돌을 확인하라
         x1, y1 = self.__point_x, self.__point_y
 
         # point 의존
-        x2, y2 = unit.__point_x, unit.__point_y
+        x2, y2 = unit.point_x, unit.point_y
         return (x2-x1)^2+(y2-y1)^2 <= (x1+x2)^2
     
     def move(self):
+        pass
+    
+    def broke(self):
         pass
 
     class Meta:
         abstract = True
 
 class PlayerStatus(NonVisible):
-    score = models.IntegerField()
-    life = models.IntegerField()
-
-    def update():
-        pass
-
+    status_val = models.IntegerField()
+    def update(self, effect):
+        self.status_val += effect
+    
     class Meta:
         abstract = True
 
 class Gun(Visible):
-    max_bullet = 3
+    max_bullet = models.IntegerField(default=3)
 
     def fire(self, angle):
-        Bullet_set.create_bullet(angle)
+        Bullet.create_bullet(angle)
 
-class Bullet(Collisible):
+class Bullet(Collapsible):
     gun = models.ForeignKey(Gun)
 
+    @classmethod
     def create_bullet(self, angle):
         return Bullet.objects.create(
             number=str(Bullet.objects.count()+1),
@@ -68,8 +71,32 @@ class Bullet(Collisible):
         self.__coo_y -= self.__speed * math.cos(math.radians(self.__angle))
         self.save()
 
-class Enemy(Collisible):
+    def broke(self):
+        # if 벽일 때, else enemy일 때...
+        pass
+
+class Enemy(Collapsible):
+    spawn_pos = models.CharField()
+
+    @classmethod
+    def create_enemy(self, angle):
+        return Enemy.objects.create(
+            number=str(Bullet.objects.count()+1),
+            __point_x=random.choice(self.spawn_pos),
+            __point_y=self.__height,
+            __angle=angle
+        )
+    
     def move(self):
         self.__point_y += self.__speed
         self.save()
+    
+    def broke(self):
+        #if 바닥일 때
+        pass
 
+class Score(PlayerStatus):
+    pass
+
+class Life(PlayerStatus):
+    pass
--- shoot/shootgame/views.py
+++ shoot/shootgame/views.py
@@ -3,9 +3,11 @@ from rest_framework.decorators import action
 from rest_framework.response import Response
 from django.views.generic import TemplateView
 from .models import *
-from .serializers import PlayerSerializer, BulletSerializer
+from .serializers import *
 import random
 import time
+import uuid
+from django.db import connection
 
 class OutFrameView(TemplateView):
     template_name = "out_frame.html"
@@ -13,23 +15,30 @@ class OutFrameView(TemplateView):
 class FrameView(TemplateView):
     template_name = "frame.html"
 
-class PlayerView(viewsets.ModelViewSet):
-    queryset = Player.objects.all()
-    serializer_class = PlayerSerializer
-        
+class PlayerViewSet(viewsets.ModelViewSet):
+    @action(detail=False, methods=['post'])
+    def start_game(self, request):
+        Player.objects.all().delete()
+        BoxEnemy.objects.all().delete()
+        Bullet.objects.all().delete()
+
+        player = Player.start_player()
+        return Response({"message": "Game Start", "player_id": player.id})
+
     @action(detail=False, methods=['post'])
     def fire(self, request):
         player = Player.objects.first()
         if not player:
             return Response({"error": "Player not found"}, status=400)
 
-        angle = int(request.data.get('angle'))
+        angle = int(request.data.get('angle'))  
+
         bullet = player.fire(angle)
         if not bullet: 
             return Response({"error": "Bullet could not be created"}, status=400)
 
         return Response(BulletSerializer(bullet).data)
-    
+
     @action(detail=False, methods=['get'])
     def status(self, request):
         player = Player.objects.first()
@@ -39,24 +48,46 @@ class PlayerView(viewsets.ModelViewSet):
         return Response({
             "score": player.score,
             "life": player.life,
+            "game_over": player.game_over()
             "game_over": player.game_over
         })
 
-class GameView(viewsets.ViewSet):
-    ctr = CreateObject()
+class BoxEnemyViewSet(viewsets.ModelViewSet):
+    queryset = BoxEnemy.objects.all()
+    serializer_class = BoxEnemySerializer
 
-    def create(self, request, *args, **kwargs):
-        player = Player.start_player()
-        enemy = self.ctr.spawn(Enemy)
-        return Response({"message": "Game Start", "player_id": player.id})
-    
     @action(detail=False, methods=['post'])
     def spawn(self, request):
-        enemy_data = self.ctr.spawn_enemy()
+        enemy = BoxEnemy.create_enemy()
         time.sleep(random.uniform(3, 5)) 
-        return Response(enemy_data)
+        return Response(BoxEnemySerializer(enemy).data)
 
     @action(detail=False, methods=['post'])
-    def tick(self, request):
-        game_state = self.ctr.update_tick()
-        return Response(game_state)
+    def move(self, request):
+        for enemy in BoxEnemy.objects.all():
+            enemy.move()
+
+            if enemy.hit_bottom():
+                enemy.broke()
+
+        return Response({"message": "Enemies moved"})
+
+class BulletViewSet(viewsets.ModelViewSet):
+    queryset = Bullet.objects.all()
+    serializer_class = BulletSerializer
+    lookup_field = "id"
+
+    @action(detail=False, methods=['post'])
+    def move(self, request):
+        bullets = Bullet.objects.all()
+        enemies = Enemy.objects.all()
+
+        for bullet in bullets:
+            bullet.move()
+
+            for enemy in enemies:
+                if bullet.hit_enemy(enemy):
+                    bullet.broke()
+                    enemy.broke()
+
+        return Response({"message": "Bullets moved"})
