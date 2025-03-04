0d843672e1d96d51f1bf334f1450378ce186fda6
hykk-git <gratia1206@naver.com>
2025-03-04 03:10:00 +0000

    refactor: 구조 변경

--- shoot/shootgame/models.py
+++ shoot/shootgame/models.py
@@ -2,78 +2,74 @@ from django.db import models
 import math
 import random
 
-class Unit(models.Model):
-    __number = models.CharField(max_length=100, blank=True, null=True)
-    __created = models.DateTimeField(auto_now_add=True)
-    __point_x = models.IntegerField()
-    __point_y = models.IntegerField()
-    __speed = models.IntegerField(default=100)
-    __angle = models.IntegerField(default=0)
-    __size = models.IntegerField()
-
-    @property
-    def point(self):
-        return self.__point_x, self.__point_y
-    
-    def broke(self):
-        self.delete()
-
-    def move(self, angle):
-        pass
-
-    def isCollision(self, ob1, ob2):
-        x1, y1 = ob1.__coo
-        x2, y2 = ob2.__coo
-        rad1 = ob1.__size
-        rad2 = ob2.__size
-        return (x2-x1)^2+(y2-y1)^2 <= (rad1+rad2)^2 
-
-    class meta:
+class GameObject:
+    class Meta:
         abstract = True
 
-class Enemy(models.Model):
-    unit = models.ForeignKey(Unit, on_delete=models.CASCADE)
-    def move(self, angle):
-        self.__point_y += self.__speed * math.sin(math.radians(angle))
+class Visible(GameObject):
+    __point_x = models.IntegerField()
+    __point_y = models.IntegerField()
+    __height = models.IntegerField()
+    __width= models.IntegerField()
+
+    class Meta:
+        abstract = True
+
+class NonVisible(GameObject):
+    class Meta:
+        abstract = True
+
+class Collisible(Visible):
+    __speed = models.IntegerField()
+    __angle = models.IntegerField()
+
+    def isCollision(self, unit):
+        x1, y1 = self.__point_x, self.__point_y
+
+        # point 의존
+        x2, y2 = unit.__point_x, unit.__point_y
+        return (x2-x1)^2+(y2-y1)^2 <= (x1+x2)^2
     
-class Bullet(models.Model):
-    unit = models.ForeignKey(Unit, on_delete=models.CASCADE)
-    def move(self, angle):
-        self.__point_x += self.__speed * math.sin(math.radians(angle))
-        self.__point_y -= self.__speed * math.cos(math.radians(angle))
-
-class Player(models.Model):
-    id = models.CharField(primary_key=True, default="1", max_length=10, editable=False)
-    bullet = models.ForeignKey(Bullet, on_delete=models.CASCADE)
-
-    def fire(self, angle):
-        current_bullet = CreateObject.spawn(self.bullet)
-        current_bullet.move(angle)
-
-class CreateObject:
-    def spawn(self, enemy):
-        return enemy.objects.create()
-    
-class PlayerInfo(models.Model):
-    __score = models.IntegerField(default=0)
-    __life = models.IntegerField(default=3)
-    player = models.OneToOneField(Player, on_delete=models.CASCADE)
-
-    def player_start(self):
-        Player.objects.all().delete()
-
-        player, _ = Player.objects.get_or_create(id=1, defaults={"name": "Player1"})
-        return player
-    
-    def update(self):
+    def move(self):
         pass
 
-    def lose_life(self, val):
-        self.__life -= val
+    class Meta:
+        abstract = True
+
+class PlayerStatus(NonVisible):
+    score = models.IntegerField()
+    life = models.IntegerField()
+
+    def update():
+        pass
+
+    class Meta:
+        abstract = True
+
+class Gun(Visible):
+    max_bullet = 3
+
+    def fire(self, angle):
+        Bullet_set.create_bullet(angle)
+
+class Bullet(Collisible):
+    gun = models.ForeignKey(Gun)
+
+    def create_bullet(self, angle):
+        return Bullet.objects.create(
+            number=str(Bullet.objects.count()+1),
+            __point_x=self.__width//2, # 중앙 생성
+            __point_y=self.__height,
+            __angle=angle
+        )
+    
+    def move(self):
+        self.__coo_x += self.__speed * math.sin(math.radians(self.__angle))
+        self.__coo_y -= self.__speed * math.cos(math.radians(self.__angle))
         self.save()
 
-    def gain_score(self, val):
-        self.__score += val
+class Enemy(Collisible):
+    def move(self):
+        self.__point_y += self.__speed
         self.save()
-    
-    
+
