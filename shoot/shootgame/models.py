56bf6b4a053274b98aed2f32741585dc7acab8d4
hykk-git <gratia1206@naver.com>
2025-03-05 02:57:17 +0000

    refactor: 인터페이스 적용

--- shoot/shootgame/models.py
+++ shoot/shootgame/models.py
@@ -1,41 +1,46 @@
 from django.db import models
 import math
 import random
+from overrides import overrides
 
 class GameObject:
+    __name = models.CharField()
+
+    @property
+    def name(self):
+        return self.name
     class Meta:
         abstract = True
 
 class Visible(GameObject):
     __point_x = models.IntegerField()
     __point_y = models.IntegerField()
-    __height = models.IntegerField()
-    __width= models.IntegerField()
 
-    class Meta:
-        abstract = True
-
-class NonVisible(GameObject):
-    class Meta:
-        abstract = True
-
-class Collapsible(Visible):
-    __speed = models.IntegerField()
-    __angle = models.IntegerField()
-
-    def isCollision(self, unit):
-        # 충돌을 확인하라
-        x1, y1 = self.__point_x, self.__point_y
-
-        # point 의존
-        x2, y2 = unit.point_x, unit.point_y
-        return (x2-x1)^2+(y2-y1)^2 <= (x1+x2)^2
+    @property
+    def point(self):
+        return self.__point_x, self.__point_y
     
-    def move(self):
+    def update(self):
+        # 자기 상태 업데이트(이동)
+        pass
+
+    class Meta:
+        abstract = True
+
+class Effect(GameObject):
+    def activate(self):
+        # 점수나 life, 반사 효과 활성화
         pass
     
-    def broke(self):
-        self.delete()
+    class Meta:
+        abstract = True
+
+class Collidable(Visible):
+    # 충돌하는 객체들은 속도가 있음(움직임)
+    __speed = models.IntegerField()
+
+    def isCollision(self, unit):
+        pass
 
     class Meta:
         abstract = True
@@ -43,10 +48,20 @@ class Collapsible(Visible):
 class Gun(Visible):
     max_bullet = models.IntegerField(default=3)
 
+    @overrides
+    def update(self):
+        x, y = Walls.point
+        self.__point_x, self.__point_y = x//2, y
+        self.save()
+     
     def fire(self, angle):
+        if Bullet.objects.count() >= self.max_bullet:
+            Bullet.objects.last().delete()
+        
         Bullet.create_bullet(angle)
 
-class Bullet(Collapsible):
+class Bullet(Collidable):
+    __angle = models.IntegerField()
     gun = models.ForeignKey(Gun)
 
     @classmethod
@@ -58,13 +73,24 @@ class Bullet(Collapsible):
             __angle=angle
         )
     
-    def move(self):
-        self.__coo_x += self.__speed * math.sin(math.radians(self.__angle))
-        self.__coo_y -= self.__speed * math.cos(math.radians(self.__angle))
+    @overrides
+    def isCollision(self, unit):
+        x1, y1 = self.__point_x, self.__point_y
+        x2, y2 = unit.point
+        return (x2-x1)^2+(y2-y1)^2 <= (x1+x2)^2
+
+    @overrides
+    def update(self):
+        x, y = self.__point_x, self.__point_y
+        x += self._speed * math.sin(math.radians(self._angle))
+        y -= self._speed * math.cos(math.radians(self._angle))
         self.save()
+    
+    def reflex(self):
+        temp_angle = self.__angle
+        self.__angle = -temp_angle
 
-
-class Enemy(Collapsible):
+class Enemy(Collidable):
     spawn_pos = models.CharField()
 
     @classmethod
@@ -76,26 +102,43 @@ class Enemy(Collapsible):
             __angle=angle
         )
     
-    def move(self):
+    @overrides
+    def isCollision(self, unit):
+        x1, y1 = self.__point_x, self.__point_y
+        x2, y2 = unit.point
+        return (x2-x1)^2+(y2-y1)^2 <= (x1+x2)^2
+
+        # view에 가서 isColl->effect 호출 
+
+    @overrides
+    def update(self):
         self.__point_y += self.__speed
         self.save()
-        
 
-class StatusCondition:
-    collapsible = models.ForeignKey(Collapsible)
-
-class PlayerStatus(NonVisible):
-    status_val = models.IntegerField()
-    status_condition = models.ForeignKey(StatusCondition)
+class Walls(Visible):
+    height = models.IntegerField(default=800) 
+    width = models.IntegerField(default=600)
 
     def update(self):
-        self.status_val += self.status_condition
-    
-    class Meta:
-        abstract = True
+        self.__point_x, self.__point_y = 0, self.width
 
-class Score(PlayerStatus):
-    pass
+class reflex(Effect):
+    # 충돌시 반사해라
 
-class Life(PlayerStatus):
-    pass
+    @overrides
+    def activate(self, unit):
+        unit.reflex()
+
+class Score(Effect):
+    status = models.IntegerField(default=0)
+
+    @overrides
+    def activate(self):
+        self.status += 1
+
+class Life(Effect):
+    status = models.IntegerField(default=3)
+
+    @overrides
+    def activate(self):
+        self.status -= 1
